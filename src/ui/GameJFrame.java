b1753f01ba8208a13fb16776862357b1a8191eff
52Hertz <1715885085@qq.com>
2023-12-24 14:19:44 +0800

    创建主界面1

---
+++ src/ui/GameJFrame.java
@@ -0,0 +1,11 @@
+package ui;
+
+import javax.swing.*;
+public class GameJFrame extends JFrame {
+
+    public GameJFrame(){
+        this.setSize(603,680);
+        this.setVisible(true);
+    }
+
+}
---
+++ src/ui/RegisterJFrame.java
@@ -0,0 +1,8 @@
+package ui;
+import javax.swing.*;
+public class RegisterJFrame extends JFrame {
+    public RegisterJFrame(){
+        this.setSize(488,500);
+        this.setVisible(true);
+    }
+}
