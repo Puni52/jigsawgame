7e842ad47f452d024edb111eb8bd0e038a1d36af
52Hertz <1715885085@qq.com>
2023-12-24 14:50:09 +0800

    2.添加菜单

--- src/App.java
+++ src/App.java
@@ -4,8 +4,8 @@ import ui.RegisterJFrame;
 
 public class App {
     public static void main(String[] args) {
-        new LoginJFrame();
-        new RegisterJFrame();
+        // new LoginJFrame();
+        // new RegisterJFrame();
         new GameJFrame();
     }
 }
